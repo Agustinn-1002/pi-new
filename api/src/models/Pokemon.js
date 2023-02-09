cd3e13f4d45990fb3668e7044c3988f660141adf
Agustinn-1002 <71684855+Agustinn-1002@users.noreply.github.com>
2023-02-08 22:37:37 -0300

    First Commit

---
+++ api/src/models/Pokemon.js
@@ -0,0 +1,52 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('pokemon', {
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    hp: {
+      type: DataTypes.FLOAT,
+      allowNull: true
+    },
+    attack: {
+      type: DataTypes.FLOAT,
+      allowNull: true
+    },
+    defense: {
+      type: DataTypes.FLOAT,
+      allowNull: true
+    },
+    speed: {
+      type: DataTypes.FLOAT,
+      allowNull: true
+    },
+    height: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+      get(){
+        return this.getDataValue('height') + 'metros';
+      }
+    },
+    weight: {
+      type: DataTypes.FLOAT,
+      allowNull: true,
+      get(){
+        return this.getDataValue('weight') + 'Kg';
+      }
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+  });
+};
