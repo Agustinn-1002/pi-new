c61685ce90c97dc3f968ad2d686f51db2309c726
Agustinn-1002 <71684855+Agustinn-1002@users.noreply.github.com>
2023-02-14 08:42:11 -0300

    demo

--- api/src/controllers/getPokemonsApi.js
+++ api/src/controllers/getPokemonsApi.js
@@ -2,6 +2,20 @@ const axios = require('axios');
 const {Pokemon , Type} = require('../db');
 
 const getApi = async () => {
-    const {data} = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=40');
+    const {data} = await axios('https://pokeapi.co/api/v2/pokemon?limit=40');
+    let info = data.results;
+    for (let e of info) {
+        const {data} = await axios(e.url)
+        e.id=data.id;
+        e.image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${data.id}.svg`;
+        e.types = data.types.map(i => i.type.name);
+        e.attack = data.stats[1]['base_stat'];
+    }
+    console.log(info);
+    return info;
+}
+
+module.exports = {
+    getApi
 }
 
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const {getApi} = require('../controllers/getPokemonsApi')
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -7,6 +8,15 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+router.get('/', async (req,res) => {
+    let info = await getApi();
+    try {
+        res.status(200).json(info);
+    } catch (error) {
+        res.json({msn:'pokemon not found...'})
+    }
+})
 
 
 module.exports = router;
